c0fed1a0638bd1c197120545fbc47645f684ebd6
Dre mf Randaci <drerandaci@gmail.com>
2018-04-04 16:56:42 -0500

    bar back tip calculations and toggle button

--- App.js
+++ App.js
@@ -15,14 +15,15 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showTips: false,
       barBackChecked: false,
       enteredTips: '',
       calculatedTips: '',
-      hours: 0,
       allShifts: [
         {shift: '6am - 1pm', hours: 7},
         {shift: '7am - 1pm', hours: 6},
         {shift: '1pm - 7pm', hours: 6},
+        {shift: '1pm - 3pm', hours: 2}
       ],
       selectedShift: {},
       baristaTips: '',
@@ -45,11 +46,18 @@ export default class App extends React.Component {
   }  
 
   calculateTips() {
-    if(this.state.enteredTips > 0 && this.state.selectedShift.hours > 0) {      
+    if(this.state.enteredTips > 0 && this.state.selectedShift.hours > 0) {
+      let bbTips;
       let enteredTips = this.state.enteredTips;
       let kitchenTips = this.state.enteredTips*0.2;
-      let barTips = (enteredTips-(kitchenTips))/2;
-      let cashTips = (enteredTips-(kitchenTips))/2;
+      
+      if(this.state.barBackChecked) {
+        bbTips = enteredTips*0.2;
+        enteredTips -= bbTips;
+      }
+
+      let baristaTips = (enteredTips-(kitchenTips))/2;
+      let cashierTips = (enteredTips-(kitchenTips))/2;
       let kitchenTipsPerHr = kitchenTips/this.state.selectedShift.hours;
       let k1Tips = this.state.selectedShift.hours === 7 ? kitchenTipsPerHr*3/2 : kitchenTipsPerHr*2/2;
       let k2Tips = this.state.selectedShift.hours === 7 ? kitchenTipsPerHr*3/2 : kitchenTipsPerHr*2/2;
@@ -59,9 +67,10 @@ export default class App extends React.Component {
       k2Tips+=k1k2k3AverageTipsAfterFirstPayout; 
 
       this.setState({
-        baristaTips: barTips, 
-        cashierTips: cashTips, 
-        // barbackTips: bbTips, 
+        showTips: true,
+        baristaTips: baristaTips, 
+        cashierTips: cashierTips, 
+        barbackTips: bbTips, 
         k1Tips: k1Tips,
         k2Tips: k2Tips,
         k3Tips: k3Tips,
@@ -93,7 +102,7 @@ export default class App extends React.Component {
 
         <Text style={styles.shifts}>Select Shift</Text>
         
-        <CheckBox
+        {/* <CheckBox
           center
           title='6am - 1pm'
           iconRight
@@ -102,7 +111,7 @@ export default class App extends React.Component {
           uncheckedIcon='add'
           checkedColor='red'
           checked={this.state.checked}
-        />
+        /> */}
 
         <View style={{height: 100}}>        
         <FlatList
@@ -138,15 +147,14 @@ export default class App extends React.Component {
           color='gold'
           backgroundColor='black'
         />
-
-        <View>
+        <View style={{display: this.state.showTips ? '' : 'none'}}>
           <Text>Barista: {this.state.baristaTips}</Text>
           <Text>Cashier: {this.state.cashierTips}</Text>
-          <Text>Barback: {this.state.barbackTips}</Text>
+          <Text style={{display: this.state.barBackChecked ? '' : 'none'}}>Barback: {this.state.barbackTips}</Text>
           <Text>K1: {this.state.k1Tips}</Text>
           <Text>K2: {this.state.k2Tips}</Text>
           <Text>K3: {this.state.k3Tips}</Text>
-      </View>
+        </View>
       </View>
     );
   }
