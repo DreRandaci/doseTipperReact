feb5c8521dba9c91f05e936b98f7b9a4bf2af0ef
Dre mf Randaci <drerandaci@gmail.com>
2018-04-04 12:00:35 -0500

    added picker element for shift selection

--- App.js
+++ App.js
@@ -3,13 +3,44 @@ import {
     StyleSheet, 
     Text, 
     View,
-    TextInput } from 'react-native';
+    TextInput,
+    FlatList,
+    Picker } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      text: 'Enter Total Tips',
+      allShifts: [
+        {shift: '6am - 1pm'},
+        {shift: '7am - 1pm'},
+        {shift: '1pm - 7pm'},
+      ],
+      selectedShift: ''
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.heading}>Dose Tipper</Text>
+        
+        <Text style={styles.heading}>Dose Tipper</Text>        
+        
+        <Picker
+          selectedValue={this.state.selectedShift}
+          onValueChange={(itemValue, itemIndex) => this.setState({selectedShift: itemValue})}
+          >          
+          {
+            this.state.allShifts.map((val, i) => 
+              <Picker.Item key={i} label={val.shift} value={val.shift}/>)
+          }          
+        </Picker>
+
+        <TextInput 
+          maxLength = {40}
+          value={this.state.text}/>
+
       </View>
     );
   }
@@ -18,8 +49,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',    
+    backgroundColor: '#fff',    
   },
   heading: {
     marginTop: 50,
